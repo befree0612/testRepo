bbb64203c8fbb8cfd3b145af60c7ba750a0f3894
shin0128 <testes0128@gmail.com>
2013-11-17 23:10:42 +0900

    test2

--- MultiScreen/MultiScreen/MultiScreenViewController.h
+++ MultiScreen/MultiScreen/MultiScreenViewController.h
@@ -2,8 +2,8 @@
 //  MultiScreenViewController.h
 //  MultiScreen
 //
-//  Created by 津崎 真一 on 13/10/20.
-//  Copyright (c) 2013年 津崎 真一. All rights reserved.
+//  Created by shin0128 on 13/10/20.
+//  Copyright (c) 2013年 shin0128. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
